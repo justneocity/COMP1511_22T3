2d4dbb79567f66038d0cb664b475ac3172dbd0b0
Rifa Jamal <z5311190@ad.unsw.edu.au>
2022-09-16 17:42:32 +1000

    week 1

---
+++ week01/hello.c
@@ -0,0 +1,32 @@
+// Hello world program
+// Written by H17A 15/09/2022
+
+/*
+ /\___/\
+(=' w '=)
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello world\n");
+
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+    // Anything after return is IGNORED
+}
+
+/*
+EXTRA:
+
+hello.c   -->   compile    -->  program
+human          translator       computer
+
+Steps to run a program:
+    1) code hello.c     
+    2) dcc hello.c -o my_program 
+    3) ./my_program
+*/
